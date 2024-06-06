9edac525bb3035d56574d727182cee31a9160e33
Itay Etelis <92247226+Etelis@users.noreply.github.com>
2024-06-06 11:09:53 +0300

    Created the LSTM encoder.

---
+++ models/lstm_encoder.py
@@ -0,0 +1,13 @@
+import torch
+from torch import nn
+
+class LSTMEncoder(nn.Module):
+    def __init__(self, embedding_size, hidden_size, num_layers, bidirectional=False):
+        super(LSTMEncoder, self).__init__()
+        self.lstm = nn.LSTM(embedding_size, hidden_size, num_layers=num_layers, bidirectional=bidirectional, batch_first=True)
+    
+    def forward(self, inp, inp_lengths):
+        packed_input = nn.utils.rnn.pack_padded_sequence(inp, inp_lengths, batch_first=True, enforce_sorted=False)
+        packed_output, hidden = self.lstm(packed_input)
+        output, _ = nn.utils.rnn.pad_packed_sequence(packed_output, batch_first=True)
+        return output, hidden
