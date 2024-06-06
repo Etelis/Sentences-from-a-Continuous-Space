abc7b1e78e8c7a60957bb2f54bcaa0369eb1c723
Itay Etelis <92247226+Etelis@users.noreply.github.com>
2024-06-06 08:56:09 +0300

    Download script.

---
+++ data/download_data.py
@@ -0,0 +1,36 @@
+
+import os
+import requests
+import tarfile
+
+def download_and_extract_data(url, extract_to):
+    # Download the file
+    response = requests.get(url)
+    tar_path = os.path.join(extract_to, 'simple-examples.tgz')
+    with open(tar_path, 'wb') as file:
+        file.write(response.content)
+
+    # Extract the tar file
+    with tarfile.open(tar_path, 'r:gz') as tar:
+        tar.extractall(path=extract_to)
+
+    # Move the relevant files to the data directory
+    data_dir = os.path.join(extract_to, 'data')
+    os.makedirs(data_dir, exist_ok=True)
+    os.rename(os.path.join(extract_to, 'simple-examples/data/ptb.train.txt'), os.path.join(data_dir, 'ptb.train.txt'))
+    os.rename(os.path.join(extract_to, 'simple-examples/data/ptb.valid.txt'), os.path.join(data_dir, 'ptb.valid.txt'))
+    os.rename(os.path.join(extract_to, 'simple-examples/data/ptb.test.txt'), os.path.join(data_dir, 'ptb.test.txt'))
+
+    # Clean up
+    os.remove(tar_path)
+    for root, dirs, files in os.walk(os.path.join(extract_to, 'simple-examples')):
+        for file in files:
+            os.remove(os.path.join(root, file))
+        for dir in dirs:
+            os.rmdir(os.path.join(root, dir))
+    os.rmdir(os.path.join(extract_to, 'simple-examples'))
+
+if __name__ == "__main__":
+    url = "http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz"
+    extract_to = "."
+    download_and_extract_data(url, extract_to)
