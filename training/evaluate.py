d144ce9b68db80ac42864a9a3bb49cce9534ee50
Itay Etelis <github@redirecto.me>
2024-06-09 10:01:55 +0300

    Update to the evaluate function to handle errors.

--- training/evaluate.py
+++ training/evaluate.py
@@ -1,6 +1,6 @@
 import torch
 
-def evaluate(model, dataloader, criterion, device):
+def evaluate(model, dataloader, criterion, device, anneal_function, step, k, annealing_till):
     model.eval()
     total_loss = 0
     with torch.no_grad():
@@ -9,7 +9,7 @@ def evaluate(model, dataloader, criterion, device):
             inputs, targets = inputs.to(device), targets.to(device)
             
             outputs, mu, logsigma, _ = model(inputs, lengths)
-            loss = criterion(outputs, targets, mu, logsigma)
+            loss, nll_loss, kl_loss, kl_weight = criterion(outputs, targets, lengths, mu, logsigma, anneal_function, step, k, annealing_till)
             total_loss += loss.item()
 
     return total_loss / len(dataloader)
